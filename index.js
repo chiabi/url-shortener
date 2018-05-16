248f8d1cbad597aa13c3e7edf0985d2ed89ef68e
chiabi <chiabi88@gmail.com>
2018-05-16 15:17:01 +0900

    morgan 적용

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// 모듈을 불러오는 코드는 대개 맨 위에 작성한다.
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use('/static', express.static('public'))
+// app.use(morgan('combined'))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+  res.render('index.ejs')
+})
 
 app.listen(3000, () => {
   console.log('listening...')
